3e978895547546b6226ab0fb867c92828e2c2162
dinghaoyuan <976069783@qq.com>
2022-08-11 14:30:28 +0800

    init

---
+++ code-artist/src/main/java/com/jojo/codeartist/CodeArtistApplication.java
@@ -0,0 +1,13 @@
+package com.jojo.codeartist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeArtistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeArtistApplication.class, args);
+    }
+
+}
---
+++ code-artist/src/test/java/com/jojo/codeartist/CodeArtistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojo.codeartist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeArtistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
