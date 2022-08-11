0ba5389e9c5009692bf0987268172fbbe06678b9
dinghaoyuan <976069783@qq.com>
2022-08-11 19:00:36 +0800

    init

--- code-artist/example/src/main/java/com/example/example/ExampleApplication.java
+++ code-artist/example/src/main/java/com/example/example/ExampleApplication.java
@@ -1,13 +1,13 @@
-package com.jojo.codeartist;
+package com.example.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeArtistApplication {
+public class ExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CodeArtistApplication.class, args);
+        SpringApplication.run(ExampleApplication.class, args);
     }
 
 }
--- code-artist/example/src/test/java/com/example/example/ExampleApplicationTests.java
+++ code-artist/example/src/test/java/com/example/example/ExampleApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jojo.codeartist;
+package com.example.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CodeArtistApplicationTests {
+class ExampleApplicationTests {
 
     @Test
     void contextLoads() {
