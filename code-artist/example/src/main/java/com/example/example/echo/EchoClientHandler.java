fad2253d14fbbe8ad91e9298ab5d0a8f7945e6be
dinghaoyuan <976069783@qq.com>
2022-08-16 19:18:16 +0800

    netty in action chapter2

---
+++ code-artist/example/src/main/java/com/example/example/echo/EchoClientHandler.java
@@ -0,0 +1,29 @@
+package com.example.example.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+import java.nio.charset.Charset;
+
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("netty rocks", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println("client received: " + msg.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
